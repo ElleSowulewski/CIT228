d492d83d204bc3084b998b4919ac76a559ed5700
Elle Sowulewski <sowulee@mail.nmc.edu>
2021-01-18 21:18:29 -0500

    initial push

---
+++ Lesson1/numberFun.py
@@ -0,0 +1,25 @@
+import random
+
+number1 = random.randrange(1, 100)
+number2 = random.randrange(1, 100)
+NUMBER2 = 25
+
+print (f"First Number = {number1}")
+print (f"Second Number = {number2}")
+print (f"{number1} / {number2} = {number1 / number2}")
+print (f"{number1} // {number2} = {number1 // number2}")
+print (f"{number1} % {number2} = {number1 % number2}")
+print (f"{number1} + {number2} = {number1 + number2}")
+print (f"{number1} - {number2} = {number1 - number2}")
+print (f"{number1} * {number2} = {number1 * number2}")
+
+print()
+
+print (f"First Number = {number1}")
+print (f"Second Number = {NUMBER2}")
+print (f"{number1} / {NUMBER2} = {number1 / NUMBER2}")
+print (f"{number1} // {NUMBER2} = {number1 // NUMBER2}")
+print (f"{number1} % {NUMBER2} = {number1 % NUMBER2}")
+print (f"{number1} + {NUMBER2} = {number1 + NUMBER2}")
+print (f"{number1} - {NUMBER2} = {number1 - NUMBER2}")
+print (f"{number1} * {NUMBER2} = {number1 * NUMBER2}")
---
+++ Lesson1/stringFun.py
@@ -0,0 +1,37 @@
+print("==============================")
+print("Exercise 1")
+name = "elle sowulewski"
+print(name.title())
+print(name.upper())
+print(name.lower())
+print("My first initial is: ",name[0].upper())
+print("==============================")
+print()
+
+print("==============================")
+print("Exercise 2")
+noun = "quail"
+adj = "loud"
+verb = "ran"
+ending = "all around today."
+sentence1 = "the " + adj + " little " + noun + " " + verb + " " + ending
+sentence2 = f"the {adj} little {noun} {verb} {ending}"
+print(sentence1.upper())
+print(sentence2.lower())
+print("==============================")
+print()
+
+print("==============================")
+print("Exercise 3")
+quote = "You gotta make a statement! You gotta look inside yourself and say: 'What am I willing to put up with today?'. Not this!!"
+print(quote)
+print("==============================")
+print()
+
+print("==============================")
+print("Exercise 4")
+lists = "\tAnimals\tGames\n\t-------\t-----\n\tCat\tSkyrim\n\tFox\tSubnautica\n\tBear\tMinecraft"
+print(lists)
+print("==============================")
+print()
+
